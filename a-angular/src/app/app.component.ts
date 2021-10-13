4f3ff133a51ef696a356b80fe6eed063381b50e6
Chyld Medford <chyld.medford@gmail.com>
2021-10-12 17:11:59 -0700

    angular sending request

--- a-angular/src/app/app.component.ts
+++ a-angular/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'a-angular';
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get(environment.API_BASE_URL + '/todos').subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
